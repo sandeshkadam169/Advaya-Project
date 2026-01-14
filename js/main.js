4d021d81631380a8d0e25562989338ee738d2655
herohunter350 <sandesh99kadam@gmail.com>
2026-01-14 21:02:43 +0530

    js updated form

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (entry.isIntersecting) {
         const counter = entry.target;
         const target = +counter.getAttribute('data-target');
-        const duration = 5000; // Animation duration in ms
+        const duration = 3000; // Animation duration in ms
         const increment = target / (duration / 16); // 60fps approx
 
         let current = 0;
@@ -42,4 +42,30 @@ document.addEventListener("DOMContentLoaded", () => {
   counters.forEach(counter => {
     observer.observe(counter);
   });
+
+
+  document.querySelector("#donationForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    fetch("https://script.google.com/macros/s/AKfycbyqZuglhgBoNQkf_W1PHQM8vtLWKZ-L6qdRBAXn_R_s24TT_TYSyjmGc-08WjHEunbq6Q/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+      },
+      body: JSON.stringify({
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        amount: document.getElementById("amount").value,
+      
+        transactionId: document.getElementById("transactionId").value
+      })
+    })
+      .then(res => res.json())
+      .then(data => alert("Donation saved successfully!"));
+  });
+
 });
+
+
+// https://script.google.com/macros/s/AKfycbygpS2DiLl6I1pe3YmRg6855eaqYuFDoJJrc9jlAoHc9vFe5hd0YMFf97iQq_JlOpBlug/exec
